c4da70d6e696f27b623e1030e402f82e11d823bd
Saurabhbash <saurabhsahare210@gmail.com>
2022-07-13 13:14:30 +0530

    redux

---
+++ src/component/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container my-4'>
+        <h2>Buy Adidas Shoes 50rs</h2>
+      <button className='btn btn-primary mx-1'>-</button>
+      Add This Item Card
+      <button className='btn btn-primary mx-1'>+</button>
+
+    </div>
+  )
+}
+
+export default Shop
